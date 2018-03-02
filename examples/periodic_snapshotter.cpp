bff6686ed14177804954fd750182dd17887e4862
Eric Schleicher <eric@constructiverealities.io>
2018-03-02 15:05:21 -0800

    changes required to make it work with pointcloud2

--- examples/periodic_snapshotter.cpp
+++ examples/periodic_snapshotter.cpp
@@ -36,10 +36,10 @@
 #include <ros/ros.h>
 
 // Services
-#include "laser_assembler/AssembleScans.h"
+#include "laser_assembler/AssembleScans2.h"
 
 // Messages
-#include "sensor_msgs/PointCloud.h"
+#include "sensor_msgs/PointCloud2.h"
 
 
 /***
@@ -58,13 +58,13 @@ public:
   PeriodicSnapshotter()
   {
     // Create a publisher for the clouds that we assemble
-    pub_ = n_.advertise<sensor_msgs::PointCloud> ("assembled_cloud", 1);
+    pub_ = n_.advertise<sensor_msgs::PointCloud2> ("assembled_cloud", 1);
 
     // Create the service client for calling the assembler
-    client_ = n_.serviceClient<AssembleScans>("assemble_scans");
+    client_ = n_.serviceClient<AssembleScans2>("assemble_scans2");
 
     // Start the timer that will trigger the processing loop (timerCallback)
-    timer_ = n_.createTimer(ros::Duration(5,0), &PeriodicSnapshotter::timerCallback, this);
+    timer_ = n_.createTimer(ros::Duration(0.5), &PeriodicSnapshotter::timerCallback, this);
 
     // Need to track if we've called the timerCallback at least once
     first_time_ = true;
@@ -82,14 +82,14 @@ public:
     }
 
     // Populate our service request based on our timer callback times
-    AssembleScans srv;
+    AssembleScans2 srv;
     srv.request.begin = e.last_real;
     srv.request.end   = e.current_real;
 
     // Make the service call
     if (client_.call(srv))
     {
-      ROS_INFO("Published Cloud with %u points", (uint32_t)(srv.response.cloud.points.size())) ;
+    ROS_INFO("Published Cloud with %u points", (uint32_t)(srv.response.cloud.row_step*srv.response.cloud.height)) ;
       pub_.publish(srv.response.cloud);
     }
     else
